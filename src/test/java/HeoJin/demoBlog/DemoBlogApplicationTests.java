009a376f729eb498e19908e3ee1aa04c8edab378
jin <hurjin1109@naver.com>
2025-03-29 19:11:39 +0900

    feat: initilization

---
+++ src/test/java/HeoJin/demoBlog/DemoBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package HeoJin.demoBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
