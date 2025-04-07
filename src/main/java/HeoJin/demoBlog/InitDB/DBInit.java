92dc799733b38e1ed23e5f8aab8e468182988a1e
jin <hurjin1109@naver.com>
2025-04-07 18:37:04 +0900

    FEAT : DB 초기화 코드 추가
    - 이후 .gitnore 등록

--- src/main/java/HeoJin/demoBlog/InitDB/DBInit.java
+++ src/main/java/HeoJin/demoBlog/InitDB/DBInit.java
@@ -1,10 +1,35 @@
 package HeoJin.demoBlog.InitDB;
 
-
+import HeoJin.demoBlog.domain.Member;
+import HeoJin.demoBlog.repository.MemberRepository;
 import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
 
 
+@RequiredArgsConstructor
+@Component
 public class DBInit {
 
+    private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
     // DB 그거
+
+    @PostConstruct
+    public void Dbinit(){
+
+        if (memberRepository.findByEmail("hurjin1109@naver.com").isEmpty()) {
+            Member member = Member.builder()
+                    .email("hurjin1109@naver.com")
+                    .password(passwordEncoder.encode("1234"))  // 나중에 .gitnore 등록하고 바꾸기
+                    .build();
+            memberRepository.save(member);
+            System.out.println("초기 사용자 데이터가 생성되었습니다.");
+        }
+
+    }
+
+
 }
