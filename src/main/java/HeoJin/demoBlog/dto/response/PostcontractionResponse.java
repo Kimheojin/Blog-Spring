cca44f218f50d71919f704dd890ed2660bce1c6e
jin <hurjin1109@naver.com>
2025-04-30 19:13:31 +0900

    FIX : 날짜 관련 직렬화 문제 해결

--- src/main/java/HeoJin/demoBlog/dto/response/PostcontractionResponse.java
+++ src/main/java/HeoJin/demoBlog/dto/response/PostcontractionResponse.java
@@ -1,6 +1,9 @@
 package HeoJin.demoBlog.dto.response;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -13,6 +16,8 @@ import java.time.LocalDateTime;
 public class PostcontractionResponse {
 
     private String title;
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
     private LocalDateTime regDate;
 }
 
--- src/test/java/HeoJin/demoBlog/controller/PostWriteControllerTest.java
+++ src/test/java/HeoJin/demoBlog/controller/PostWriteControllerTest.java
@@ -1,20 +1,38 @@
 package HeoJin.demoBlog.controller;
 
+import HeoJin.demoBlog.domain.Member;
 import HeoJin.demoBlog.dto.request.PostRequest;
 import HeoJin.demoBlog.dto.response.PostcontractionResponse;
+import HeoJin.demoBlog.util.CustomUserDetail;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import configuration.WithMockCustomUser;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 class PostWriteControllerTest {
 
     @Autowired
@@ -40,15 +58,29 @@ class PostWriteControllerTest {
                 .title("테스트 제목")
                 .regDate(LocalDateTime.now())
                 .build();
+
+
     }
 
+    private static String contextEmail() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        return (String) authentication.getPrincipal();
+    }
+
+    @Test
+    @DisplayName("post /api/post -> 게시글 작성 정상 작동 테스트")
+    @WithMockCustomUser
+    void test1() throws Exception {
 
 
-    
-    
-    
 
-
+        mockMvc.perform(post("/api/post")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(postRequest)))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
 
 
 }
