52fc5f17c836f2892a3faab4d76e26a059d44aae
jin <hurjin1109@naver.com>
2025-06-01 19:39:49 +0900

    FEAT : Comment entity 추가

---
+++ src/main/java/HeoJin/demoBlog/comment/entity/Comment.java
@@ -0,0 +1,31 @@
+package HeoJin.demoBlog.comment.entity;
+
+import HeoJin.demoBlog.post.entity.Post;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+    @Email
+    private String email;
+    private String password;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Comment parent;
+
+}
