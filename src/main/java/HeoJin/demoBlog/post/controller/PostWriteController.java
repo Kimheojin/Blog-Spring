de107064023b6067b93e32eba4fa38bdf22bc809
jin <hurjin1109@naver.com>
2025-06-02 19:35:48 +0900

    FEAT : 게시글 수정 로직 변경 및 게시글 삭제 로직 추가

--- src/main/java/HeoJin/demoBlog/post/controller/PostWriteController.java
+++ src/main/java/HeoJin/demoBlog/post/controller/PostWriteController.java
@@ -2,10 +2,12 @@ package HeoJin.demoBlog.post.controller;
 
 
 import HeoJin.demoBlog.global.util.CustomUserDetail;
+import HeoJin.demoBlog.post.dto.request.PostDeleteRequest;
 import HeoJin.demoBlog.post.dto.request.PostModifyRequest;
 import HeoJin.demoBlog.post.service.PostWriteService;
 import HeoJin.demoBlog.post.dto.request.PostRequest;
-import HeoJin.demoBlog.post.dto.response.PostcontractionResponse;
+import HeoJin.demoBlog.post.dto.response.PostContractionResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -13,7 +15,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/")
+@RequestMapping("/api/admin")
 @RequiredArgsConstructor
 public class PostWriteController {
 
@@ -22,16 +24,31 @@ public class PostWriteController {
 
     // 게시글 작성
     @PreAuthorize("hasAuthority('ADMIN')")
-    @PostMapping("/post")
-    public ResponseEntity<PostcontractionResponse> writePost(@AuthenticationPrincipal CustomUserDetail userDetail,
-                                                             @RequestBody PostRequest postDto) {
+    @PostMapping("/posts")
+    public ResponseEntity<PostContractionResponse> writePost(@AuthenticationPrincipal CustomUserDetail userDetail,
+                                                             @RequestBody @Valid  PostRequest postDto) {
         return ResponseEntity.ok(postWriteService.writePost(userDetail.getMember(), postDto));
     }
 
     // 게시글 수정
     @PreAuthorize("hasAuthority('ADMIN')")
-    @PutMapping("/post")
-    public ResponseEntity<PostcontractionResponse> modifyPost(@RequestBody PostModifyRequest postModifyRequest){
-        return ResponseEntity.ok(postWriteService.upadatePost(postModifyRequest));
+    @PutMapping("/posts")
+    public ResponseEntity<PostContractionResponse> modifyPost(@AuthenticationPrincipal CustomUserDetail userDetail,
+            @RequestBody @Valid PostModifyRequest postModifyRequest){
+        return ResponseEntity.ok(postWriteService.updatePost(postModifyRequest));
     }
+
+    // 게시글 삭제
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @DeleteMapping("/posts")
+    public ResponseEntity<String> DeletePost(@AuthenticationPrincipal CustomUserDetail userDetail,
+                                       @RequestBody PostDeleteRequest postDeleteRequest){
+        postWriteService.deletePost(postDeleteRequest);
+        return ResponseEntity.ok("게시글이 성골적으로 삭제 되었습니다.");
+    }
+
+
+    // 연관 포스트 조회
+
+    // 예약 발행
 }
