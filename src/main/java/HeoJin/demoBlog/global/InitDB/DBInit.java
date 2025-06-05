28ba71e7da48ae911ee235e217e2171e84dc2d64
jin <hurjin1109@naver.com>
2025-06-05 19:44:07 +0900

    FIX : 누락된 DBInit 파일 추가

---
+++ src/main/java/HeoJin/demoBlog/global/InitDB/DBInit.java
@@ -0,0 +1,81 @@
+package HeoJin.demoBlog.global.InitDB;
+
+import HeoJin.demoBlog.member.entity.Member;
+import HeoJin.demoBlog.member.entity.Role;
+import HeoJin.demoBlog.member.repository.MemberRepository;
+import HeoJin.demoBlog.member.repository.RoleRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.env.Environment;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Component
+@Slf4j
+public class DBInit implements CommandLineRunner {
+
+    private final MemberRepository memberRepository;
+    private final RoleRepository roleRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+    private final Environment environment;
+
+
+    @Autowired(required = false)
+    private TestDbInit testDbInit;
+
+    @Override
+    public void run(String... args) throws Exception {
+        initRoleAndUser();
+        if (Arrays.asList(environment.getActiveProfiles()).contains("local")) {
+            testDbInit.createCategory();
+            testDbInit.createPost();
+            testDbInit.createComment();
+        }
+    }
+
+    // postConstruct -> commandLineRunner
+    @Transactional
+    protected void initRoleAndUser(){
+        final String INIT_EMAIL = "hurjin1109@naver.com";
+        final String INIT_ROLE = "ADMIN";
+
+        Optional<Member> existedMember = memberRepository.findByEmail(INIT_EMAIL);
+
+        if(existedMember.isPresent()){
+            log.info("초기 사용자가 이미 존재합니다.{}", existedMember.get().getEmail() );
+            return;
+        }
+
+        Role adminRole = roleRepository.findByRoleName(INIT_ROLE)
+                .orElseGet(() -> {
+                    log.info("ADMIN 역할을 생성합니다.");
+                    return roleRepository.save(Role.builder()
+                            .roleName(INIT_ROLE)
+                            .build());
+                });
+
+
+        Member member = Member.builder()
+                .email(INIT_EMAIL)
+                .password(passwordEncoder.encode("1234"))
+                .memberName("허진")
+                .role(adminRole)
+                .build();
+
+
+        memberRepository.save(member);
+
+
+        log.info("초기 사용자 데이터가 생성되었습니다: {}", INIT_EMAIL);
+    }
+
+
+
+}
