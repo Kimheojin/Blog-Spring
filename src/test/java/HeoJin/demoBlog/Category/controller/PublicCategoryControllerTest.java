db05d8f234229b3bf0bcc80d9faca9681ec38bc6
jin <hurjin1109@naver.com>
2025-06-13 18:43:52 +0900

    DEPLOY : local profile 제거

--- src/test/java/HeoJin/demoBlog/Category/controller/PublicCategoryControllerTest.java
+++ src/test/java/HeoJin/demoBlog/Category/controller/PublicCategoryControllerTest.java
@@ -1,7 +1,6 @@
 package HeoJin.demoBlog.Category.controller;
 
 import HeoJin.demoBlog.configuration.base.SaveTestData;
-import HeoJin.demoBlog.member.entity.Member;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
