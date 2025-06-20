34a625663cf11a1bdb9892d24c92f8a7e1953c52
jin <hurjin1109@naver.com>
2025-06-20 19:40:02 +0900

    FIX : findAllCategoriesWithCount -> cout 로직 수정

--- src/main/java/HeoJin/demoBlog/category/repository/CategoryRepositoryImpl.java
+++ src/main/java/HeoJin/demoBlog/category/repository/CategoryRepositoryImpl.java
@@ -6,6 +6,7 @@ import HeoJin.demoBlog.category.entity.QCategory;
 import HeoJin.demoBlog.post.entity.PostStatus;
 import HeoJin.demoBlog.post.entity.QPost;
 import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -18,20 +19,21 @@ public class CategoryRepositoryImpl implements CategoryRepositoryCustom{
 
     private final JPAQueryFactory jpaQueryFactory;
 
-
     @Override
     public List<CategoryWithCountDto> findAllCategoriesWithCount() {
         QCategory category = QCategory.category;
         QPost post = QPost.post;
+
         return jpaQueryFactory
                 .select(Projections.constructor(CategoryWithCountDto.class,
                         category.id,
                         category.categoryName,
-                        post.id.count()))
+                        JPAExpressions
+                                .select(post.count())
+                                .from(post)
+                                .where(post.category.eq(category)
+                                        .and(post.status.eq(PostStatus.PUBLISHED)))))
                 .from(category)
-                .leftJoin(post)
-                .on(post.category.eq(category), post.status.eq(PostStatus.PUBLISHED))
-                .groupBy(category.id, category.categoryName)
                 .orderBy(category.categoryName.asc())
                 .fetch();
     }
