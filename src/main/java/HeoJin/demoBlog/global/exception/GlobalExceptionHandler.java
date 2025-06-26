9dc20a2bad08be94ac5ee121b4d07200c29d4d2a
jin <hurjin1109@naver.com>
2025-06-26 19:02:01 +0900

    FEAT : Bean Validation 관련 예외 annotation 조건 추가 및 Gelboal exception 처리 관련 코드 추가

--- src/main/java/HeoJin/demoBlog/global/exception/GlobalExceptionHandler.java
+++ src/main/java/HeoJin/demoBlog/global/exception/GlobalExceptionHandler.java
@@ -2,7 +2,8 @@ package HeoJin.demoBlog.global.exception;
 
 
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,7 +17,6 @@ public class GlobalExceptionHandler {
         ErrorResponse response = ErrorResponse.builder()
                 .message(e.getMessage())
                 .statusCode(statusCode)
-                .code(String.valueOf(statusCode))  // statusCode를 code로 변환
                 .build();
 
         // validation 데이터가 있으면 복사
@@ -25,5 +25,21 @@ public class GlobalExceptionHandler {
         }
 
         return ResponseEntity.status(statusCode).body(response);
+    }
+
+    // @Valid 관련 예외 처리
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException e) {
+        ErrorResponse response = ErrorResponse.builder()
+                .message("입력값 검증에 실패했습니다.")
+                .statusCode(400)
+                .build();
+
+        // 필드별 오류 메시지 추가 되는듯
+        for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+            response.addValidation(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        return ResponseEntity.badRequest().body(response);
     }
 }
