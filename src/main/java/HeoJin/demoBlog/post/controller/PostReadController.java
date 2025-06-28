2dcb010ec266110414c33084627fed28c513886f
jin <hurjin1109@naver.com>
2025-06-28 21:06:05 +0900

    FEAT : loggin 관려 aop 메소드 및 추가
    - application loggin level 설정
    - global 부분 annotation 기반 aop 로깅 로직 추가
    - 필터단까지는 추가 X

--- src/main/java/HeoJin/demoBlog/post/controller/PostReadController.java
+++ src/main/java/HeoJin/demoBlog/post/controller/PostReadController.java
@@ -10,9 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api")
-@RequiredArgsConstructor
+    @RestController
+    @RequestMapping("/api")
+    @RequiredArgsConstructor
 public class PostReadController {
     private final PostReadService postReadService;
 
