5ddd14b7c911a47cbfcce0384227ae6db9424af1
jin <hurjin1109@naver.com>
2025-07-03 18:05:24 +0900

    SSTYLE : profile 활성화

--- src/main/java/HeoJin/demoBlog/image/controller/ImageController.java
+++ src/main/java/HeoJin/demoBlog/image/controller/ImageController.java
@@ -7,9 +7,8 @@ import HeoJin.demoBlog.image.dto.response.UploadResponse;
 import HeoJin.demoBlog.image.service.ImageService;
 import HeoJin.demoBlog.image.util.ImageUtil;
 import lombok.RequiredArgsConstructor;
-
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -21,7 +20,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/admin")
 @RequiredArgsConstructor
-//@Profile("image")
+@Profile("image")
 public class ImageController {
     
 /*
