89109c3bcc357fff4c449716c193441196b17fd6
Jin <hurjin1109@naver.com>
2025-09-05 16:09:49 +0900

    TEST : get 관리자 commentlist에 대한 doc test코드 작성

--- src/test/java/HeoJin/demoBlog/comment/controller/doc/AdminCommentControllerTest.java
+++ src/test/java/HeoJin/demoBlog/comment/controller/doc/AdminCommentControllerTest.java
@@ -34,7 +34,7 @@ class AdminCommentControllerTest extends SaveTestData {
 
     @Test
     @WithMockCustomUser
-    @DisplayName("get /api/admin/posts/{postId}/comments -> 관리자 전체 댓글 조회")
+    @DisplayName("get /api/admin/posts/{postId}/comments -> 해단 post 관리자 전체 댓글 조회")
     void test1() throws Exception {
         // given
         Post existingPost = postRepository.findAll().get(0);
@@ -109,5 +109,35 @@ class AdminCommentControllerTest extends SaveTestData {
                         fieldWithPath("commentDtoList[].parentId").description("부모 댓글 ID (최상위 댓글인 경우 null)"),
                         subsectionWithPath("commentDtoList[].replies").description("대댓글 목록 (구조는 상위 댓글과 동일)")
                 )));
+    }
+
+        @Test
+        @WithMockCustomUser
+        @DisplayName("get /api/admin/comments -> 관리자 전체 댓글 목록 조회")
+        void test3() throws Exception {
+            // given
+
+
+            // when + then
+            ResultActions testMock = mockMvc.perform(get("/api/admin/comments")
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk())
+                    .andDo(print());
+
+            // docs
+            testMock.andDo(document("get-/api/admin/comments",
+                    preprocessRequest(prettyPrint()),
+                    preprocessResponse(prettyPrint()),
+                    responseFields(
+                            fieldWithPath("commentDtoList").description("댓글 목록"),
+                            fieldWithPath("commentDtoList[].id").description("댓글 ID"),
+                            fieldWithPath("commentDtoList[].content").description("댓글 내용"),
+                            fieldWithPath("commentDtoList[].regDate").description("댓글 작성 시간"),
+                            fieldWithPath("commentDtoList[].email").description("작성자 이메일"),
+                            fieldWithPath("commentDtoList[].status").description("댓글 상태 (ACTIVE, DELETED, ADMIN_DELETED)"),
+                            fieldWithPath("commentDtoList[].postId").description("포스트 ID"),
+                            fieldWithPath("commentDtoList[].parentId").description("부모 댓글 ID (최상위 댓글인 경우 null)"),
+                            subsectionWithPath("commentDtoList[].replies").description("대댓글 목록 (구조는 상위 댓글과 동일)")
+                    )));
     }
 }
