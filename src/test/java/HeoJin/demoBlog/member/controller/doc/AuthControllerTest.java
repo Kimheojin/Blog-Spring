29215e14c0403376a01657b4e9874aa81d9e93a0
Jin <hurjin1109@naver.com>
2025-10-19 18:29:34 +0900

    TEST : Login 관련 docs 테스트 코드 추가

--- src/test/java/HeoJin/demoBlog/member/controller/doc/AuthControllerTest.java
+++ src/test/java/HeoJin/demoBlog/member/controller/doc/AuthControllerTest.java
@@ -1,50 +1,66 @@
 package HeoJin.demoBlog.member.controller.doc;
 
 import HeoJin.demoBlog.configuration.Integration.SaveTestData;
+import HeoJin.demoBlog.configuration.mockUser.WithMockCustomUser;
+import HeoJin.demoBlog.member.dto.request.LoginDto;
+import HeoJin.demoBlog.member.entity.Member;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.ResultActions;
 
-import java.util.List;
-
+import static org.springframework.restdocs.cookies.CookieDocumentation.cookieWithName;
+import static org.springframework.restdocs.cookies.CookieDocumentation.responseCookies;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+
+@SpringBootTest
 public class AuthControllerTest extends SaveTestData {
+
+
+
+    @BeforeEach
+    void init(){
+        // role 까지만 생성
+        Member member =createTestMember();
+    }
 
     @Test
     @DisplayName("GET /api/auth/status - 인증된 사용자 상태 확인")
+    @WithMockCustomUser()
     void test1() throws Exception {
+        // WithMock 타면 filter 인증 관련 코드 안탐
         // given
-        Authentication authentication = getMockAuthentication();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        // when
-        ResultActions resultActions = mockMvc.perform(get("/api/auth/status")
+        Long expectedMemberId = (Long) authentication.getPrincipal();
+
+
+        // when + then
+        ResultActions testMock = mockMvc.perform(get("/api/auth/status")
                         .with(SecurityMockMvcRequestPostProcessors.authentication(authentication)) // 시큐리티 컨텍스트에 Authentication 객체 설정
-                        .contentType(MediaType.APPLICATION_JSON));
+                        .contentType(MediaType.APPLICATION_JSON))
+                        .andExpect(status().isOk())
+                        .andExpect(jsonPath("$.authenticated").isBoolean())
+                        .andExpect(jsonPath("$.memberId").value(expectedMemberId))
+                        .andExpect(jsonPath("$.role").value("ADMIN"))
+                        .andDo(print());
 
-        // then
-        resultActions
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.authenticated").value(true))
-                .andExpect(jsonPath("$.memberId").value(1L))
-                .andExpect(jsonPath("$.role").value("ROLE_USER"))
-                .andDo(print());
+
 
         // docs
-        resultActions.andDo(document("get-auth-status-authenticated",
+        testMock.andDo(document("get-auth-status-authenticated",
                 preprocessRequest(prettyPrint()),
                 preprocessResponse(prettyPrint()),
                 responseFields(
@@ -56,34 +72,47 @@ public class AuthControllerTest extends SaveTestData {
     }
 
     @Test
-    @DisplayName("GET /api/auth/status - 인증되지 않은 사용자 상태 확인")
+    @DisplayName("POST /api/auth/login - 사용자 로그인 테스트")
     void test2() throws Exception {
-        // given (인증 정보 없음)
+        // given
 
-        // when
-        ResultActions resultActions = mockMvc.perform(get("/api/auth/status")
-                .contentType(MediaType.APPLICATION_JSON));
+        LoginDto loginDto = LoginDto.builder()
+                .email(TEST_EMAIL)
+                .password(TEST_PASSWORD).build();
 
-        // then
-        resultActions
-                .andExpect(status().isUnauthorized())
-                .andExpect(jsonPath("$.authenticated").value(false))
+        // when + then
+        ResultActions testMock = mockMvc.perform(post("/api/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loginDto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("로그인 성공"))
+                .andExpect(jsonPath("$.statusCode").value(200))
+                .andExpect(cookie().exists("accessToken"))
+                .andExpect(cookie().httpOnly("accessToken", true))
+                .andExpect(cookie().path("accessToken", "/"))
                 .andDo(print());
 
         // docs
-        resultActions.andDo(document("get-auth-status-unauthenticated",
+        testMock.andDo(document("post-/api/auth/login",
                 preprocessRequest(prettyPrint()),
                 preprocessResponse(prettyPrint()),
+                requestFields(
+                        fieldWithPath("email").description("사용자 이메일"),
+                        fieldWithPath("password").description("사용자 비밀번호")
+                ),
                 responseFields(
-                        fieldWithPath("authenticated").description("인증 여부 (false)"),
-                        fieldWithPath("message").description("응답 메시지")
-                )));
+                        fieldWithPath("message").description("로그인 성공"),
+                        fieldWithPath("statusCode").description("상태 코드")
+                ),
+                responseCookies(
+                        cookieWithName("accessToken").description("HttpOnly, Path=/로 설정된 액세스 토큰")
+                )
+        ));
+
     }
 
-    private Authentication getMockAuthentication() {
 
-        Long memberId = 1L;
-        List<GrantedAuthority> authorities = AuthorityUtils.createAuthorityList("ADMIN");
-        return new UsernamePasswordAuthenticationToken(memberId, null, authorities);
-    }
+
+
+
 }
